42aa55a36e28612e9c2edb53034547bc54e98968
nishanthelango <nishanthelango@yahoo.com.sg>
2019-08-25 18:45:31 +0800

    added ability to mark tasks as done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,18 @@ public class Duke {
     private static final String HORIZONTAL_LINE = "    ____________________________________________________________";
     private static final String INDENTATION = "     ";
 
-    private static List<String> taskList = new ArrayList<>();
+    private static List<Task> taskList = new ArrayList<>();
+
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+        greetUser();
+        reply();
+    }
 
     private static void printIndented(String s) {
         System.out.println(INDENTATION + s);
@@ -34,26 +45,48 @@ public class Duke {
                 return;
             }
             if (input.equals("list")) {
+                printIndented("Here are the tasks in your list:");
                 for (int i = 1; i <= taskList.size(); i++) {
-                    printIndented(i + ". " + taskList.get(i-1));
+                    printIndented(i + ".[" + taskList.get(i-1).getStatusIcon()
+                            + "] "+ taskList.get(i-1).getDescription());
                 }
             }
+
             else {
-                taskList.add(input);
-                printIndented("added: " + input);
+                String[] line = input.split(" ");
+                if (line.length == 2 && line[0].equals("done") && isNumeric(line[1])) {
+                    int i = Integer.parseInt(line[1]);
+                    if (i > 0 && i <= taskList.size()) {
+                        printIndented("Nice! I've marked this task as done:");
+                        taskList.get(i-1).markAsDone();
+                        printIndented("[✓] "+ taskList.get(i-1).getDescription());
+                    }
+                    else {
+                        printIndented("Task not found");
+                    }
+                }
+                else {
+                    taskList.add(new Task(input));
+                    printIndented("added: " + input);
+                }
             }
             printLine();
         }
     }
 
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        greetUser();
-        reply();
+    private static boolean isNumeric(String s) {
+        try {
+            int i = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
     }
+
+
+
+
+
 }
+
+
