0fe85e16f6b02c215760802291ec6f1578b34ed2
nishanthelango <nishanthelango@yahoo.com.sg>
2019-08-25 17:12:22 +0800

    Duke greets user,echos commands and exits with bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,42 @@
+import java.util.Scanner;
+
 public class Duke {
+    private static final String HORIZONTAL_LINE = "    ____________________________________________________________";
+    private static final String INDENTATION = "     ";
+
+    private static void printIndented(String s) {
+        System.out.println(INDENTATION + s);
+    }
+
+    private static void printLine() {
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    private static void greetUser() {
+        printLine();
+        printIndented("Hello! I'm Duke");
+        printIndented("What can I do for you?");
+        printLine();
+    }
+
+    private static void reply() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                printLine();
+                printIndented("Bye. Hope to see you again soon!");
+                printLine();
+                return;
+            }
+            else {
+                printLine();
+                printIndented(input);
+                printLine();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +44,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greetUser();
+        reply();
     }
 }
